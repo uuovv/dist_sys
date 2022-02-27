b96713bb33ace758eb0afe651e67dd1621e92dc9
Александр Карнаухов <aleksandrkarnauhov@192.168.1.5>
2022-02-27 11:55:47 +0300

    Changed file Achive_2/Program/requirements.txt; Changed file Achive_2/Program/setup.sh

--- Achive_2/Program/setup.sh
+++ Achive_2/Program/setup.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
 sudo apt-get update;
-sudo apt-get install python3-venv python3-pip libpq-dev;
-sudo apt install python3-venv python3-pip libpq-dev;
+sudo apt-get -y install python3.8 python3-venv python3-pip libpq-dev;
 sudo pip install --upgrade pip;
-
-python3 -m venv venv;
