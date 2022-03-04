03fdcf18fbdfff76a6cffbfa3b6eab0e53060a28
Александр Карнаухов <aleksandrkarnauhov@192.168.1.5>
2022-03-04 15:43:28 +0300

    Changed files dbserver/Dockerfile, dbserver/envlist_dbserver.env, docker-compose-web-db.yaml, webserver/Dockerfile, webserver/envlist_webserver.env, webserver/main.py

--- Achive_2/Program/webserver/main.py
+++ Achive_2/Program/webserver/main.py
@@ -1,7 +1,8 @@
-import os
+import  os
 import psycopg as pg3
 import socket
 import json
+from time import sleep
 
 DBNAME = os.environ['DBNAME']
 DB_USER = os.environ['DB_USER']
@@ -14,98 +15,108 @@ SERVER_HOST = os.environ['SERVER_HOST']
 SERVER_PORT = int(os.environ['SERVER_PORT'])
 SOCKET_SIZE = int(os.environ['SOCKET_SIZE'])
 
+WAIT_S = int(os.environ['WAIT_S'])
+
 #-------------------------------------------------------------------------------------
 
 def main():
-    create_db(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD)
-    create_table(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD)
+    create_db(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
+    create_table(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
 
     server = create_server(SERVER_HOST, SERVER_PORT)
     try:
         while True: 
             client_socket, address = server.accept()
             http_num = resive_data(client_socket, SOCKET_SIZE)
-            db_num = get_number(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD)
+            condition = check_condition(http_num, DBNAME, DB_USER, 
+                                        DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
             
-            print('Полученное число: {}; Число в базе: {}'.format(http_num, db_num))
+            print('http_num: {};'.format(http_num))
         
-            insert_f, content = condition(http_num, db_num)
+            insert_f, content = condition_processing(http_num, condition)
             
             if insert_f:
-                insert_to_table(http_num, DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD)
+                insert_to_table(http_num, DBNAME, DB_USER, 
+                                DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
                 send_data(client_socket, content)
             else:
                 send_data(client_socket, content)
                 
             print(content)
     except KeyboardInterrupt:
-        print('Сервер отключен по вашей просьбе.')
+        print('Server turned off.')
     except Exception as e: 
-        print('Произошла ошибка.\n{}'.format(e))
+        print('Exception\n{}'.format(e))
     finally:
         server.close()    
 
 #-------------------------------------------------------------------------------------
 
-def create_db(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD):
-    with pg3.connect(dbname=DBNAME, user=DB_USER, host=DB_HOST, port=DB_PORT, 
-                    password=DB_PASSWORD, autocommit=True) as conn:
-        with conn.cursor() as cur:
+def create_db(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S):
+    conn = wait_until_connect(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
+    with conn.cursor() as cur:
+        cur.execute('''
+            SELECT datname FROM pg_database;
+                    ''')
+    
+        db_list = cur.fetchall()
+        conn.commit()
+    
+        if (DBNAME,) not in db_list:
             cur.execute('''
-                    SELECT datname FROM pg_database;
+                CREATE DATABASE achive_2;
                         ''')
-    
-            db_list = cur.fetchall()
-            conn.commit()
-    
-            if (DBNAME,) not in db_list:
-                cur.execute('''
-                    CREATE DATABASE achive_2;
-                            ''')
     conn.close()    
 
 
-def create_table(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD):
-    with pg3.connect(dbname=DBNAME, user=DB_USER, host=DB_HOST, 
-                    port=DB_PORT, password=DB_PASSWORD) as conn:
-        with conn.cursor() as cur:
-            cur.execute('''
-                    CREATE TABLE IF NOT EXISTS numbers (
-                        id serial NOT NULL PRIMARY KEY,
-                        num int NOT NULL
-                        );
-                        ''')            
+def create_table(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S):
+    conn = wait_until_connect(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
+    with conn.cursor() as cur:
+        cur.execute('''
+            CREATE TABLE IF NOT EXISTS numbers (
+                id serial NOT NULL PRIMARY KEY,
+                num int NOT NULL
+                );
+                    ''')            
     conn.close()
 
-def get_number(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD):
-    with pg3.connect(dbname=DBNAME, user=DB_USER, host=DB_HOST, 
-                    port=DB_PORT, password=DB_PASSWORD) as conn:
-        with conn.cursor() as cur:
-            cur.execute('''
-                    SELECT n1.num
-                    FROM numbers n1
-                    WHERE n1.id = (SELECT MAX(n2.id) FROM numbers n2);
-                        ''' )            
-            number = cur.fetchone()
+def check_condition(http_num, DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S):
+    conn = wait_until_connect(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
+    with conn.cursor() as cur:
+        cur.execute('''
+            SELECT SUM(
+                CASE WHEN num={0} THEN 1
+                    WHEN num={1} THEN 2
+                    ELSE 0
+                    END)
+                FROM numbers;
+                    '''.format(http_num, http_num+1))            
+        condition = cur.fetchone()
     conn.close()
 
-    if type(number) == type(None):
-        return number
-    else:
-        return number[0]
+    return condition[0]
 
-def insert_to_table(num, DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD):
-    with pg3.connect(dbname=DBNAME, user=DB_USER, host=DB_HOST, 
-                    port=DB_PORT, password=DB_PASSWORD) as conn:
-        with conn.cursor() as cur:
-            cur.execute('''
-                        INSERT INTO numbers (num)
-                        VALUES
-                        (%s);
-                        ''', (num,))            
-            conn.commit()
+def insert_to_table(num, DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S):
+    conn = wait_until_connect(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
+    with conn.cursor() as cur:
+        cur.execute('''
+            INSERT INTO numbers (num)
+            VALUES
+            (%s);
+                    ''', (num,))            
+        conn.commit()
     conn.close()
 
+def wait_until_connect(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S):
+    while True:
+        try:
+            conn = pg3.connect(dbname=DBNAME, user=DB_USER, host=DB_HOST, 
+                            port=DB_PORT, password=DB_PASSWORD)
+            break
+        except pg3.errors.OperationalError: 
+            sleep(WAIT_S)
+    return conn
+    
 #-------------------------------------------------------------------------------------
 
 def create_server(SERVER_HOST, SERVER_PORT):
@@ -123,19 +134,18 @@ def send_data(client_socket, content):
     client_socket.send(HDRS.encode('utf-8') + content.encode('utf-8'))
     client_socket.shutdown(socket.SHUT_WR)
 
-def condition(http_num, db_num):
-    if ((db_num == None and http_num >= 0) or
-        (http_num != db_num and http_num != db_num - 1 and http_num >= 0)):
+def condition_processing(http_num, condition):
+    if (condition == 0 and http_num >= 0):
         insert_f = True
-        content = 'Результат обработки числа: {}\n\r'.format(http_num+1)
+        content = 'Result of processing the http_num: {}\n\r'.format(http_num+1)
     else:
         insert_f = False
         if http_num < 0:
-            content = 'Ошибка: Число должно быть больше или равно 0.\n\r' 
-        elif http_num != db_num - 1:
-            content = 'Ошибка: Число уже было обработано.\n\r'
-        elif http_num != db_num:
-            content = 'Ошибка: Число меньше обработанного числа на 1.\n\r'
+            content = 'Error: The number must be greater than or equal to 0.\n\r' 
+        elif (condition == 1 or condition == 3):
+            content = 'Error: The number has already been processed.\n\r'
+        elif condition == 2:
+            content = 'Error: The number is less than the processed number by 1.\n\r'
     
     return (insert_f, content)
 
