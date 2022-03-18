0cc0dd39db4fec805870e72eb954b3e38dcd34f6
Александр Карнаухов <aleksandrkarnauhov@192.168.1.5>
2022-03-19 00:18:07 +0300

    Changed files: Achive_2/Program/k8s/deploy-svc-ing-web.yaml; Achive_2/Program/webserver/main.py

--- Achive_2/Program/webserver/main.py
+++ Achive_2/Program/webserver/main.py
@@ -2,6 +2,7 @@ import os
 import psycopg as pg3
 import socket
 import json
+import logging
 from time import sleep
 
 DBNAME = os.environ['DBNAME']
@@ -17,6 +18,8 @@ SOCKET_SIZE = int(os.environ['SOCKET_SIZE'])
 
 WAIT_S = int(os.environ['WAIT_S'])
 
+logging.basicConfig(level=logging.DEBUG)
+
 #-------------------------------------------------------------------------------------
 
 def main():
@@ -27,9 +30,6 @@ def main():
             http_num = resive_data(client_socket, SOCKET_SIZE)
             condition = check_condition(http_num, DBNAME, DB_USER, 
                                         DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
-            
-            print('Received number: {};'.format(http_num))
-        
             insert_f, content = condition_processing(http_num, condition)
             
             if insert_f:
@@ -39,31 +39,34 @@ def main():
             else:
                 send_data(client_socket, content)
                 
-            print(content)
     except KeyboardInterrupt:
-        print('Server turned off.')
+        logging.debug('Server turned off.')
     except Exception as e: 
-        print('Exception\n{}'.format(e))
+        logging.debug('Exception: {}'.format(e))
     finally:
         server.close()
 
 #-------------------------------------------------------------------------------------
 
 def check_condition(http_num, DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S):
-    conn = wait_until_connect(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
-    with conn.cursor() as cur:
-        cur.execute('''
-            SELECT SUM(
-                CASE WHEN num={0} THEN 1
-                    WHEN num={1} THEN 2
-                    ELSE 0
-                    END)
-                FROM numbers;
-                    '''.format(http_num, http_num+1))            
-        condition = cur.fetchone()
-    conn.close()
-    print('check_condition OK')
-    return condition[0]
+    if (http_num != None and http_num >= 0):
+        conn = wait_until_connect(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
+        with conn.cursor() as cur:
+            cur.execute('''
+                SELECT SUM(
+                    CASE WHEN num={0} THEN 1
+                        WHEN num={1} THEN 2
+                        ELSE 0
+                        END)
+                    FROM numbers;
+                        '''.format(http_num, http_num+1))            
+            condition = cur.fetchone()
+        conn.close()
+        logging.debug('check_condition OK')
+        return condition[0]
+    else:
+        logging.debug('check_condition OK')
+        return None
 
 def insert_to_table(num, DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S):
     conn = wait_until_connect(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
@@ -75,18 +78,18 @@ def insert_to_table(num, DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S)
                     ''', (num,))            
         conn.commit()
     conn.close()
-    print('insert_to_table OK')
+    logging.debug('insert_to_table OK')
 
 def wait_until_connect(DBNAME, DB_USER, DB_HOST, DB_PORT, DB_PASSWORD, WAIT_S):
     while True:
         try:
             conn = pg3.connect(dbname=DBNAME, user=DB_USER, host=DB_HOST, 
                             port=DB_PORT, password=DB_PASSWORD)
-            print('wait_until_connect OK')
+            logging.debug('wait_until_connect OK')
             break
         except pg3.errors.OperationalError: 
             sleep(WAIT_S)
-            print('wait_until_connect NOT OK')
+            logging.debug('wait_until_connect NOT OK')
     return conn
     
 #-------------------------------------------------------------------------------------
@@ -95,38 +98,49 @@ def create_server(SERVER_HOST, SERVER_PORT):
     server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server.bind((SERVER_HOST, SERVER_PORT))
     server.listen(5)
-    print('create_server OK')
+    logging.debug('create_server OK')
     return server
 
 def resive_data(client_socket, SOCKET_SIZE):
-    data = client_socket.recv(SOCKET_SIZE).decode('utf-8').split('\r\n')[-1]
-    num = int(json.loads(data)['num'])
-    print('resive_data OK')
-    return num
+    try:
+        data = client_socket.recv(SOCKET_SIZE).decode('utf-8').split('\r\n')[-1]
+        num = int(json.loads(data)['num']) 
+        return num
+    except json.JSONDecodeError:
+        num = None
+        return num
+    finally:
+        logging.debug(f'resive_data OK')
+        logging.debug(f'data: {data}')
+        logging.debug(f'num: {num}')
 
 def send_data(client_socket, content):
     try:
-        client_socket.send(HDRS.encode('utf-8') + content.encode('utf-8'))
+        client_socket.send(HDRS.encode('utf-8') + content.encode('utf-8') + '\r\n'.encode('utf-8'))
         client_socket.shutdown(socket.SHUT_WR)
-        print('send_data OK')
     except OSError as e:
-        print('Exception\r\n{}'.format(e.args[1]))
+        logging.debug('Exception: {}'.format(e.args[1]))
     finally:
         client_socket.close()
+        logging.debug('send_data OK')
 
 def condition_processing(http_num, condition):
-    if ((condition == 0 or condition == None) and http_num >= 0):
+    if ((condition == 0 or condition == None) and (http_num != None and http_num >= 0)):
         insert_f = True
-        content = '{}\r\n'.format({"num": http_num+1})
+        content = '{}'.format({"num": http_num+1})
     else:
         insert_f = False
-        if http_num < 0:
-            content = 'Error: The number must be greater than or equal to 0.\n\r' 
+        if http_num == None:
+            content = 'Error: Incorrect input data.'
+        elif http_num < 0:
+            content = 'Error: The number must be greater than or equal to 0.' 
         elif (condition == 1 or condition == 3):
-            content = 'Error: The number has already been processed.\n\r'
+            content = 'Error: The number has already been processed.'
         elif condition == 2:
-            content = 'Error: The number is less than the processed number by 1.\n\r'
-    print('condition_processing OK')
+            content = 'Error: The number is less than the processed number by 1.'
+    logging.debug('condition_processing OK')
+    logging.debug(f'insert_f: {insert_f}')
+    logging.debug(f'content: {content}')
     return (insert_f, content)
 
 #-------------------------------------------------------------------------------------
